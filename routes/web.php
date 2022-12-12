cce32ed9812a1370afbb079702c5b96d5ba34912
Kerem Karşıyaka <keremkarsiyakaa@gmail.com>
2022-12-12 13:55:56 +0300

    Initial Commit

---
+++ routes/web.php
@@ -0,0 +1,59 @@
+<?php
+
+use App\Http\Controllers\ListingController;
+use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Models\Listing;
+
+
+//all listings
+Route::get('/', [ListingController::class, 'index']);
+
+
+//show create form
+Route::get('/listings/create', [ListingController::class, 'create'])->middleware('auth');
+
+
+//store listing data
+Route::post('/listings', [ListingController::class, 'store'])->middleware('auth');
+
+
+//show edit form
+Route::get('/listings/{listing}/edit', [ListingController::class, 'edit'])->middleware('auth');
+
+
+//update listings
+Route::put('/listings/{listing}', [ListingController::class, 'update'])->middleware('auth');
+
+
+//delete listings
+Route::delete('/listings/{listing}', [ListingController::class, 'destroy'])->middleware('auth');
+
+
+//manage listings
+Route::get('/listings/manage', [ListingController::class, 'manage'])->middleware('auth');
+
+
+//single listing
+Route::get('/listings/{listing}', [ListingController::class, 'show']);
+
+
+//show register/create form
+Route::get('/register', [UserController::class, 'create'])->middleware('guest');
+
+
+//create new user
+Route::post('/users', [UserController::class, 'store']);
+
+
+//log user Out
+Route::post('/logout', [UserController::class, 'logout'])->middleware('auth');
+
+
+//show login form
+Route::get('/login', [UserController::class, 'login'])->name('login')->middleware('guest');
+
+
+//login user
+Route::post('/users/authenticate', [UserController::class, 'authenticate']);
